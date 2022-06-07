29bb3bcf5bad4eacff0c6d0a01a18f8140ce3e87
Chandruxworks <chandrashekharayyah.xworkz@gmail.com>
2022-06-07 21:19:21 +0530

    Star pattern program

---
+++ java_core/src/com/xworkz/java/patternprogram/starpattern/Pattern11.java
@@ -0,0 +1,25 @@
+package com.xworkz.java.patternprogram;
+
+public class Pattern11 {
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++)
+		{
+			for(int j=10;j>i;j--)
+			{
+		      System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++)
+			{
+				if(i>=2 && k>1)
+				{
+					 System.out.print(" ");
+				}
+			else
+			{
+				System.out.print("*");
+			}
+			}
+			System.out.println();
+	}
+}
+}
