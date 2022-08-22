ec1389dbdf94f34e152e2f17e1ab126c102e2d2e
Chandruxworks <chandrashekharayyah.xworkz@gmail.com>
2022-08-22 12:02:41 +0530

    validator service

--- market/src/com/xworkz/market/util/Factory.java
+++ market/src/com/xworkz/market/util/Factory.java
@@ -1,8 +1,9 @@
 package com.xworkz.market.util;
 
-
+import javax.persistence.EntityManagerFactory;
 
 public class Factory {
+	
 	private static EntityManagerFactory factory;
 	
 	public static EntityManagerFactory getFactory() {
